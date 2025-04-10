ea0e5e3d6da0d5161661508dc21d3822bc532df0
marwan-elghitany-cntxt <marwan.elghitany@cntxt.tech>
2025-04-10 18:24:15 +0400

    updating the req and form paths

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ ADGM Process Automation
 
 ## Run the app
 <!-- for fixing import issues -->
-> export PYTHONPATH=.  
-> cd adgm_cases  
-> streamlit run app/main.py  
+- export PYTHONPATH=.  
+- cd adgm_cases  
+- streamlit run app/main.py  
+
+### App:
+
+https://adgm-auto.streamlit.app/
--- adgm_cases/app/constants.py
+++ adgm_cases/app/constants.py
@@ -1,5 +1,7 @@
 # Constants
-EMPLOYMENT_FORM_PATH = "/Users/marwan.elghitany/work/repos/adgm-cases/adgm_cases/forms/form_employment_v2.json"
-CLAIM_FORM_PATH = "/Users/marwan.elghitany/work/repos/my-notebooks/notebooks/research/adgm/forms/form_claim.json"
+# EMPLOYMENT_FORM_PATH = "/Users/marwan.elghitany/work/repos/adgm-cases/adgm_cases/forms/form_employment_v2.json"
+# CLAIM_FORM_PATH = "/Users/marwan.elghitany/work/repos/my-notebooks/notebooks/research/adgm/forms/form_claim.json"
+EMPLOYMENT_FORM_PATH = "/workspaces/adgm-cases/adgm_cases/forms/form_employment.json"
+CLAIM_FORM_PATH = "/workspaces/adgm-cases/adgm_cases/forms/form_claim.json"
 
 TEMP_DIR = "users"
